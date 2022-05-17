29286ea743732c0ef2468a1f1c433c298a0df7d4
Matts52 <matt.s5@hotmail.com>
2022-05-17 14:40:52 -0600

    google analytics

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import VueGtag from "vue-gtag";
 
-createApp(App).mount('#app')
+createApp(App).use(VueGtag, {
+  config: { id: "UA-228976612-1" }
+}).mount("#app");
+
+
+
+//createApp(App).mount('#app')
