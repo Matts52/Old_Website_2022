17bedfa5acf559ec2a6d130db663c8b2324d5003
Matts52 <matt.s5@hotmail.com>
2022-05-18 15:34:49 -0600

    attempting to fix google analytics

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import VueGtag from "vue-gtag";
 
 createApp(App).use(VueGtag, {
-  config: { id: "UA-228976612-1" }
+  config: { id: "G-9FNDMCEFFC" }
 }).mount("#app");
 
 
